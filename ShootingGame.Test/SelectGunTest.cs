b88741589ca6742ce5a47bd0f7d23b818661de49
Ajay311 <46600867+Ajay311@users.noreply.github.com>
2019-12-24 00:06:01 +0530

    Add files via upload

---
+++ ShootingGame.Test/SelectGunTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace ShootingGame.Test
+{
+    [TestClass]
+    public class SelectGunTest
+    {
+        [TestMethod]
+        public void SelectGun_Pistol()
+        {
+            string userInput = "1";
+            using (StringReader sr = new StringReader(userInput))
+            {
+                Console.SetIn(sr);
+                Gun currgun = Program.SelectGun();
+                Console.Write(currgun.Name);
+                Assert.AreEqual<string>(Constant.Pistol, currgun.Name);
+            }
+        }
+
+        [TestMethod]
+        public void SelectGun_Rifle()
+        {
+            string userInput = "2";
+            using (StringReader sr = new StringReader(userInput))
+            {
+                Console.SetIn(sr);
+                Gun currgun = Program.SelectGun();
+                Assert.AreEqual<string>(Constant.Rifle, currgun.Name);
+            }
+        }
+
+        [TestMethod]
+        public void SelectGun_Invalid_Number()
+        {
+            string userInput = "4";
+            using (StringReader sr = new StringReader(userInput))
+            {
+                Console.SetIn(sr);
+                Gun currgun = Program.SelectGun();
+                Assert.IsNull(currgun);
+            }
+        }
+
+        [TestMethod]
+        public void SelectGun_Invalid_Char()
+        {
+            string userInput = "abc";
+            using (StringReader sr = new StringReader(userInput))
+            {
+                Console.SetIn(sr);
+                Gun currgun = Program.SelectGun();
+                Assert.IsNull(currgun);
+            }
+        }
+    }
+}
